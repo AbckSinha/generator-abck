5103ae1fc4879c9e61d334d8836df774c234e985
AbckSinha <abhishek.sinha132@gmail.com>
2018-07-18 15:56:00 +0530

    added scripts for running nodejs commands

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,8 +4,11 @@ const chalk = require('chalk');
 const yosay = require('yosay');
 var path = require('path');
 var fs = require('fs');
+var npmRun = require('npm-run');
+var npm = require('npm');
 
 var prompts = require('./sources/prompts.js');
+var shell = require('./sources/shellHelper.js');
 
 var MyAppGenerator = class extends Generator {
 
@@ -17,6 +20,8 @@ var MyAppGenerator = class extends Generator {
       this.log(chalk.red('Non-empty directory. Cordova needs an empty directory to set up project'));
       process.exit(1);
     }
+
+
   }
 
   prompting() {
@@ -25,25 +30,96 @@ var MyAppGenerator = class extends Generator {
       yosay(`Welcome to Full Fledged Ionic2 ${chalk.red('generator-abck')} generator!`)
     );
 
+    // const prompts = [{
+    //   type: 'input',
+    //   name: 'projectName',
+    //   message: 'Enter a name for your project',
+    //   default: this.appname                        //default to current folder
+    // },{
+    //   type: 'checkbox',
+    //   name: 'appType',
+    //   message: 'Specify app type ?',
+    //   choices: [{
+    //     name: 'Blank',
+    //     value: 'includeBlank',
+    //     checked: true
+    //   },{
+    //     name: 'Tabs',
+    //     value: 'includeTabs',
+    //     checked: false
+    //   },{
+    //     name: 'Sidemenu',
+    //     value: 'includeSideMenu',
+    //     checked: false
+    //   }]
+    // }];
+
     return this.prompt(prompts.prompts).then(props => {
       // To access props later use this.props.projectName;
       this.props = props;
-      if(props.appType.length>1){
+      if (props.appType.length > 1) {
         this.log(chalk.red('Please select only one app type'));
         process.exit(1);
       }
+
+
+      // execute multiple commands in series
+      // shell.series([
+
+     //  npmRun.exec(['npm', 'install', 'ionic', '-g'].join(' '));
+     //  npmRun.exec(['ionic', 'start', 'myapp', 'tabs'].join(' '));
+     //  npmRun.exec(['bower','install' ].join(' '));
+       // 'npm install ionic --save',
+       // 'ionic start myApp tabs'
+      // ], function (err) {
+      //   console.log('executed many commands in a row');
+      // });
+
+      // npmRun.exec('npm install ionic --save', 'ionic start myApp tabs', (err) => {
+      //   this.log(
+      //     chalk.green(`Installed Ionic`)
+      //   );
+      // });
+
+      npmRun.exec('ionic start myApp tabs', (err) => {
+        this.log(
+          chalk.green(`creating cordova project ======>>>>>>>>>`)
+        );
+      });
+      done();
     });
   }
 
   writing() {
     this.fs.copy(
       this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+      this.destinationPath('dummyfile.txt'),
+      this.templatePath('package.json'),
+      this.destinationPath('package.json')
     );
   }
 
   install() {
-    this.installDependencies();
+     let installConf = {
+      npm: true,
+      skipInstall: false
+    };
+
+    this.installDependencies(installConf);
+
+    // npmRun.exec('npm install', (err) => {
+    //     this.log(
+    //       chalk.green(`installing dependencies ...`)
+    //     );
+    //   });
+  }
+
+    end() {
+    this.log(yosay(
+      'All done! To get going run:\n' +
+      chalk.green('go to root of folder and run  \n') +
+      'ng serve'
+    ));
   }
 }
 
